14d8d8e157da22081da6177862bab372ca0a50d6
Yerin <myheaven_b@naver.com>
2023-07-20 15:56:45 +0900

    feat: 2주차 실습 : Task Manager

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -4,33 +4,66 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.taskcompleted.ui.theme.TaskCompletedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            TaskCompletedScreen()
+        }
     }
 }
 
 @Composable
 fun TaskCompletedScreen() {
-    Column( ) { }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        CompleteImage(painterResource(id = R.drawable.ic_task_completed))
+        CompleteMessage(stringResource(id = R.string.all_task_completed))
+        CheerMessage(stringResource(id = R.string.nice_work))
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun CompleteImage(image: Painter) {
+    Image(painter = image, contentDescription = "")
+}
+
+@Composable
+fun CompleteMessage(message: String) {
+    Text(
+        text = message,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+    )
+}
+
+@Composable
+fun CheerMessage(message: String) {
+    Text(
+        text = message,
+        fontSize = 16.sp,
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    TaskCompletedScreen()
+}
