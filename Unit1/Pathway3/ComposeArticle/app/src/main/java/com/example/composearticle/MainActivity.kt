82b8e9856dfa64464560f58de9b81a2c10a02b51
Yerin <88828858+RightHennessy@users.noreply.github.com>
2023-07-27 19:15:50 +0900

    [WEEK2] 실습 (#2)
    * chore: 버전 변경
    
    * feat: 2주차 실습: 컴포즈 아티클
    
    * feat: 2주차 실습 : Task Manager
    
    * feat: 2주차 실습: 컴포즈 사분명

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -5,31 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleApp()
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+    val painter = painterResource(id = R.drawable.bg_compose_background)
+    ArticleCard(
+        title = "Jetpack Compose tutorial",
+        shortDescription = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+        longDescription = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+        imagePainter = painter,
+    )
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +45,54 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column() {
+        Banner(imagePainter)
+        Title(title, modifier)
+        ShortDescription(shortDescription, modifier)
+        LongDescription(longDescription, modifier)
+    }
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+private fun Banner(imagePainter: Painter) {
+    Image(
+        painter = imagePainter,
+        contentDescription = null,
+        alignment = Alignment.TopCenter,
+    )
+}
+
+@Composable
+private fun Title(message: String, modifier: Modifier) {
+    Text(
+        text = message,
+        modifier = modifier.padding(16.dp),
+        fontSize = 24.sp,
+    )
+}
+
+@Composable
+private fun ShortDescription(message: String, modifier: Modifier) {
+    Text(
+        text = message,
+        modifier = modifier.padding(
+            horizontal = 16.dp,
+        ),
+        textAlign = TextAlign.Justify,
+    )
+}
+
+@Composable
+private fun LongDescription(message: String, modifier: Modifier) {
+    Text(
+        text = message,
+        modifier = modifier.padding(16.dp),
+        textAlign = TextAlign.Justify,
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ComposeArticleApp()
+}
--- Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,33 +4,72 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.style.TextAlign.Companion.Justify
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            MaterialTheme() {
+                ComposeQuadrantApp()
+            }
+        }
     }
 }
 
 @Composable
 fun ComposeQuadrantApp() {
-    Column() {
-        Row() { }
-        Row() { }
+    val modifier = Modifier.fillMaxHeight()
+    Column {
+        Row(
+            modifier.weight(1f),
+        ) {
+            ComposableInfoCard(
+                title = stringResource(id = R.string.first_title),
+                description = stringResource(id = R.string.first_description),
+                backgroundColor = Color.Green,
+                modifier = modifier.weight(1f),
+            )
+
+            ComposableInfoCard(
+                title = stringResource(id = R.string.second_title),
+                description = stringResource(id = R.string.second_title),
+                backgroundColor = Color.Yellow,
+                modifier = modifier.weight(1f),
+            )
+        }
+        Row(
+            modifier.weight(1f),
+        ) {
+            ComposableInfoCard(
+                title = stringResource(id = R.string.third_title),
+                description = stringResource(id = R.string.third_title),
+                backgroundColor = Color.Cyan,
+                modifier = modifier.weight(1f),
+            )
+
+            ComposableInfoCard(
+                title = stringResource(id = R.string.fourth_title),
+                description = stringResource(id = R.string.fourth_description),
+                backgroundColor = Color.LightGray,
+                modifier = modifier.weight(1f),
+            )
+        }
     }
 }
 
@@ -39,12 +78,37 @@ private fun ComposableInfoCard(
     title: String,
     description: String,
     backgroundColor: Color,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    Column( ) { }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.background(backgroundColor).padding(16.dp),
+    ) {
+        TitleText(message = title)
+        DescriptionText(message = description)
+    }
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun TitleText(message: String) {
+    Text(
+        text = message,
+        fontWeight = Bold,
+        modifier = Modifier.padding(bottom = 16.dp),
+    )
+}
+
+@Composable
+fun DescriptionText(message: String) {
+    Text(
+        text = message,
+        textAlign = Justify,
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ComposeQuadrantApp()
+}
